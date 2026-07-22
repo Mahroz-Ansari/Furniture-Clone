3217aecd4d5ab9d1655ac84347701f411d3f29ed
Mahroz-Ansari <mahroz2203ansari@gmail.com>
2026-07-21 14:08:45 -1200

    initial commit

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import hero from './assets/hero-banner.png';
 
 const Hero = () => {
     return (
@@ -14,7 +15,7 @@ const Hero = () => {
                         </div>
                     </div>
                     <div className='hero-image'>
-                        <img className='w-120 h-100' src="assets/hero-banner.png" alt="" />
+                        <img className='w-120 h-100' src={hero} alt="" />
                     </div>
                 </div>
             </div>
